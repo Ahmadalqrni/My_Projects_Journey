27e9ba49b1ba3b140718ddd82797bfd522060f3f
lazy_owl <ahmadalqrni2002@gmail.com>
2026-02-17 14:57:36 +0300

    finsh the first step of weather app (not completed yet)

--- progress/weather/script.js
+++ progress/weather/script.js
@@ -2,9 +2,41 @@ Temperature = document.getElementById("Temperature-disply");
 cityname = document.getElementById("cityname");
 wetherinput = document.getElementById("weather_input");
 weatherget = document.getElementById("getWeather");
+speedwind = document.getElementById('windspeed');
 
-weatherget.addEventListener("click", function () {
+weatherget.addEventListener("click", async function () {
+
   let city = wetherinput.value;
-  let url = `http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no`;
-  fetch(url);
+
+  if (!city) {
+    alert("Enter a city name");
+    return;
+  }
+
+  // Get coordinates
+  let geoResponse = await fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${city}`
+  );
+  let geoData = await geoResponse.json();
+
+  if (!geoData.results) {
+    alert("City not found");
+    return;
+  }
+
+  let { latitude, longitude } = geoData.results[0];
+
+  // Get weather
+  let weatherResponse = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+  );
+  let weatherData = await weatherResponse.json();
+
+  Temperature.textContent =
+    weatherData.current_weather.temperature + "°C";
+
+    speedwind.textContent = 'speed wind: '+ weatherData.current_weather.windspeed;
+
+  cityname.textContent = geoData.results[0].name;
+
 });
